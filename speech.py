beb2a26cbd7ef4433b4f31ec3662201a46f0b7f3
JharnaSaxena <J9610577999@GMAIL.COM>
2026-03-12 22:40:54 +0530

    fix: remove duplicate voices, add language support
    - Convert kokoro_voices list to a set to automatically eliminate
      duplicate entries (am_adam, am_echo, am_eric, am_fenrir were
      listed twice)
    
    - Remove duplicate PITCH_MIN = 0 constant definition
    
    - Add LANGUAGE_CODES dictionary mapping language names to their
      correct Kokoro lang_code values (English, Hindi, French, Italian,
      Portuguese, Japanese, Chinese)
    
    - Add set_language() method to Speech class that reinitializes the
      Kokoro pipeline with the selected language code and tracks
      current language via self.current_language
    
    - Initialize self.current_language = 'English' in __init__ to
      match the default lang_code='a' used in setup_kokoro()
    
    Fixes #31, relates to #27

--- speech.py
+++ speech.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2009, Aleksey Lim
 # Copyright (C) 2019, Chihurumnaya Ibiam <ibiamchihurumnaya@sugarlabs.org>
-# Copyright (C) 2025, Mebin J Thattil <mail@mebin.in>
+# Copyright (C) 2025, Mebin J Thattil <mail@mebin.in> 
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
